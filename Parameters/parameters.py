fe063778dc0a0e17cf909318eb1939a1c4cd3e50
Hendrik Vogel <hvogel@rhel6-64f.slac.stanford.edu>
2018-05-15 17:44:10 -0700

    v0.2

--- Parameters/parameters.py
+++ Parameters/parameters.py
@@ -6,9 +6,12 @@ import numpy as np
 """
 Parameters
 enList 		Grid of energy values in TeV for which the evolution is computed
+galaxy_model    Model for the redshift evolution of the galaxies. Currently we have implemented 'Yuksel' and 'Schober'
+flux_model      Model for the neutrino spectrum. Currently we have implemented 'Kopper' and 'Nieder' (Niederhausen analysis)
 """
 
 enList = 10**np.arange(0,4.1,0.1)
-
+galaxy_model = 'Schober'
+flux_model = 'Nieder'
 
 
